51cd8fa1688b6a95d90b5d374989ce2e168fa58c
hoang28092000 <hvu6097@gmail.com>
2025-05-11 00:15:07 +0700

    update pj kinsman clohtes

---
+++ .history/js/main_20250509232633.js
@@ -0,0 +1,39 @@
+const sliders = document.querySelectorAll(".slide");
+let current = 0;
+let interval = null;
+
+function showNextSlide() {
+  sliders[current].classList.remove("active");
+  current = (current + 1) % sliders.length;
+  sliders[current].classList.add("active");
+}
+function startSlider() {
+  interval = setInterval(showNextSlide, 2500);
+}
+function stopSlider() {
+  clearInterval(interval);
+}
+
+
+
+
+const slider = document.getElementById("slider");
+slider.addEventListener("mouseover", stopSlider);
+slider.addEventListener("mouseleave", startSlider);
+
+const id = new URLSearchParams(window.location.search).get("id");
+
+fetch("./data/products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const product = data.find((item) => item.id === id);
+    if (product) {
+      document.querySelector(".product-img").src = product.image;
+      document.querySelector(".product-name").textContent = product.name;
+      document.querySelector(".product-price").textContent = product.price;
+      document.querySelector(".product-description").textContent =
+        product.description;
+    } else {
+      document.body.innerHTML = "<p>Sản phẩm không tồn tại.</p>";
+    }
+  });
